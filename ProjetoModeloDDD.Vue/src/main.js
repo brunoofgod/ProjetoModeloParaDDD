ffc30acb335698562295ddd4e8476e2079bdd8aa
brunoofgod <brunoofgod@gmail.com>
2017-07-18 00:08:55 -0300

    adicionado vue material

--- ProjetoModeloDDD.Vue/src/main.js
+++ ProjetoModeloDDD.Vue/src/main.js
@@ -3,8 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.css'
 
 Vue.config.productionTip = false
+Vue.use(VueMaterial)
 
 /* eslint-disable no-new */
 new Vue({
